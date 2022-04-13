bdddbf5f536b6adc93c1de54d1f25bdc2e837fc8
Anabel <villalobos1998@gmail.com>
2022-04-12 22:22:44 -0700

    skeleton added

---
+++ react-app/src/components/Events.js
@@ -0,0 +1,9 @@
+
+
+export const Events = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ react-app/src/components/Tasks.js
@@ -0,0 +1,10 @@
+
+
+
+export default Tasks = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
